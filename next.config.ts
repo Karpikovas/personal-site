bd908ff403e61b42e79bbb28de32dd955418de9c
Sofi Karpikova <karpikova-sp@activebt.ru>
2024-10-24 10:48:53 +0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/personal-site",
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
   output: "export", 
   reactStrictMode: true,
 };
