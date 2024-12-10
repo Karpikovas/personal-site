051d3df7466ba781814fad39166fcdd305461794
Sofi Karpikova <karpikova-sp@activebt.ru>
2024-12-10 14:37:10 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,6 @@ const nextConfig: NextConfig = {
   output: "export", 
   reactStrictMode: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
