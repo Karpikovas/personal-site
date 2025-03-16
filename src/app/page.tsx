15c5672949f6a717309627569115668d781823c6
SShekotihin <sshekotihindev@gmail.com>
2025-03-16 23:06:19 +0300

    chore: поправил текст

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <div className="container mt-8 xl:mt-16 mb-4 xl:mb-8 mx-auto px-8 lg:px-20 fadeIn1">
         <div className="flex flex-col items-center mb-8 md:mb-10 xl:mb-16">
           <p className="text-sm md:text-lg xl:text-2xl  !text-stone-300">
-            <b>Leyla Romanova</b> {siteConfig.description.slice(14)}
+            <b>Leyla Romanova</b> is a composer who creates music of different genres – from orchestral, chamber and neoclassical piano music to trance-techno.
           </p>
         </div>
         <Music />
