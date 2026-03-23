7ca42047a1b003723a0a97e7fa3aaa68133fc272
Sofi Karpikova <karpikova-sp@activebt.ru>
2026-03-23 11:13:04 +0300

    design

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,14 +1,17 @@
 // @ts-nocheck
 import { getGroups } from "@/constants/data";
 import { Group } from "./Group";
+import { Reveal } from "./Reveal";
 
 export const Music = () => {
   const groups = getGroups();
 
   return (
     <>
-      {Object.keys(groups).map((key) => (
-        <Group key={key} name={key} items={groups[key].items} type="single" isMain={groups[key].isMain} />
+      {Object.keys(groups).map((key, index) => (
+        <Reveal key={key} delay={index * 120}>
+          <Group name={key} items={groups[key].items} type="single" isMain={groups[key].isMain} />
+        </Reveal>
       ))}
     </>
   );
