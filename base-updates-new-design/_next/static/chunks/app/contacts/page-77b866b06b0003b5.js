e55989c7942418809a95bf3c3ac597860750b0c0
Karpikovas <Karpikovas@users.noreply.github.com>
2026-03-23 07:56:16 +0000

    deploy: 8e019232f052ebe2ad9b0597fdc47980cbe05776

---
+++ base-updates-new-design/_next/static/chunks/app/contacts/page-77b866b06b0003b5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[613],{4658:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4613,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[680,274,417,744],()=>s(4658)),_N_E=e.O()}]);
---
+++ base-updates-new-design/_next/static/chunks/pages/_app-26c422e30906fbcc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{6539:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4849)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[774,179],()=>(n(6539),n(8521))),_N_E=_.O()}]);
