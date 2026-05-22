46c494eebed67f87ac4acb314458ea497e4ceb95
Sofi Karpikova <karpikova-sp@activebt.ru>
2026-05-22 13:31:53 +0300

    test

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -7,8 +7,5 @@ set -eux
 # Останавливаем и удаляем старый контейнер
 ./deploy/stop.sh
 
-# Удаляем volume со старой сборкой Next.js (БАЗА ДАННЫХ НЕ ТРОГАЕТСЯ!)
-docker volume rm site_next_data
-
 # Запускаем новый контейнер (первый запуск пересоберёт Next.js)
 ./deploy/start.sh
